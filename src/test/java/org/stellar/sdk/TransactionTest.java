a6f5386e8f41979abf593a92ff56cd263825ca55
Bartek Nowotarski <bartek@nowotarski.info>
2016-02-25 11:39:21 +0100

    Missing tests

---
+++ src/test/java/org/stellar/sdk/TransactionTest.java
@@ -0,0 +1,129 @@
+package org.stellar.sdk;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class TransactionTest {
+
+  public void tearDown() {
+    Network.useTestNetwork();
+  }
+
+  @Test
+  public void testBuilderSuccessTestnet() throws FormatException {
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+    KeyPair destination = KeyPair.fromAccountId("GDW6AUTBXTOC7FIKUO5BOO3OGLK4SF7ZPOBLMQHMZDI45J2Z6VXRB5NR");
+
+    long sequenceNumber = 2908908335136768L;
+    Account account = new Account(source, sequenceNumber);
+    Transaction transaction = new Transaction.Builder(account)
+            .addOperation(new CreateAccountOperation.Builder(destination, "2000").build())
+            .build();
+
+    transaction.sign(source);
+
+    assertEquals(
+            "AAAAAF7FIiDToW1fOYUFBC0dmyufJbFTOa2GQESGz+S2h5ViAAAAZAAKVaMAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAA7eBSYbzcL5UKo7oXO24y1ckX+XuCtkDsyNHOp1n1bxAAAAAEqBfIAAAAAAAAAAABtoeVYgAAAEDLki9Oi700N60Lo8gUmEFHbKvYG4QSqXiLIt9T0ru2O5BphVl/jR9tYtHAD+UeDYhgXNgwUxqTEu1WukvEyYcD",
+            transaction.toEnvelopeXdrBase64());
+
+    assertEquals(transaction.getSourceAccount(), source);
+    assertEquals(transaction.getSequenceNumber(), sequenceNumber+1);
+    assertEquals(transaction.getFee(), 100);
+  }
+
+  @Test
+  public void testBuilderMemoText() throws FormatException {
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+    KeyPair destination = KeyPair.fromAccountId("GDW6AUTBXTOC7FIKUO5BOO3OGLK4SF7ZPOBLMQHMZDI45J2Z6VXRB5NR");
+
+    Account account = new Account(source, 2908908335136768L);
+    Transaction transaction = new Transaction.Builder(account)
+            .addOperation(new CreateAccountOperation.Builder(destination, "2000").build())
+            .addMemo(Memo.text("Hello world!"))
+            .build();
+
+    transaction.sign(source);
+
+    assertEquals(
+            "AAAAAF7FIiDToW1fOYUFBC0dmyufJbFTOa2GQESGz+S2h5ViAAAAZAAKVaMAAAABAAAAAAAAAAEAAAAMSGVsbG8gd29ybGQhAAAAAQAAAAAAAAAAAAAAAO3gUmG83C+VCqO6FztuMtXJF/l7grZA7MjRzqdZ9W8QAAAABKgXyAAAAAAAAAAAAbaHlWIAAABAxzofBhoayuUnz8t0T1UNWrTgmJ+lCh9KaeOGu2ppNOz9UGw0abGLhv+9oWQsstaHx6YjwWxL+8GBvwBUVWRlBQ==",
+            transaction.toEnvelopeXdrBase64());
+  }
+
+  @Test
+  public void testBuilderSuccessPublic() throws FormatException {
+    Network.usePublicNetwork();
+
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+    KeyPair destination = KeyPair.fromAccountId("GDW6AUTBXTOC7FIKUO5BOO3OGLK4SF7ZPOBLMQHMZDI45J2Z6VXRB5NR");
+
+    Account account = new Account(source, 2908908335136768L);
+    Transaction transaction = new Transaction.Builder(account)
+            .addOperation(new CreateAccountOperation.Builder(destination, "2000").build())
+            .build();
+
+    transaction.sign(source);
+
+    assertEquals(
+            "AAAAAF7FIiDToW1fOYUFBC0dmyufJbFTOa2GQESGz+S2h5ViAAAAZAAKVaMAAAABAAAAAAAAAAAAAAABAAAAAAAAAAAAAAAA7eBSYbzcL5UKo7oXO24y1ckX+XuCtkDsyNHOp1n1bxAAAAAEqBfIAAAAAAAAAAABtoeVYgAAAEDzfR5PgRFim5Wdvq9ImdZNWGBxBWwYkQPa9l5iiBdtPLzAZv6qj+iOfSrqinsoF0XrLkwdIcZQVtp3VRHhRoUE",
+            transaction.toEnvelopeXdrBase64());
+  }
+
+  @Test
+  public void testToBase64EnvelopeXdrBuilderNoSignatures() throws FormatException, IOException {
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+    KeyPair destination = KeyPair.fromAccountId("GDW6AUTBXTOC7FIKUO5BOO3OGLK4SF7ZPOBLMQHMZDI45J2Z6VXRB5NR");
+
+    Account account = new Account(source, 2908908335136768L);
+    Transaction transaction = new Transaction.Builder(account)
+            .addOperation(new CreateAccountOperation.Builder(destination, "2000").build())
+            .build();
+
+    try {
+      transaction.toEnvelopeXdrBase64();
+      fail();
+    } catch (RuntimeException exception) {
+      assertTrue(exception.getMessage().contains("Transaction must be signed by at least one signer."));
+    }
+  }
+
+  @Test
+  public void testNoOperations() throws FormatException, IOException {
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+
+    Account account = new Account(source, 2908908335136768L);
+    try {
+      Transaction transaction = new Transaction.Builder(account).build();
+      fail();
+    } catch (RuntimeException exception) {
+      assertTrue(exception.getMessage().contains("At least one operation required"));
+    }
+  }
+
+  @Test
+  public void testTryingToAddMemoTwice() throws FormatException, IOException {
+    // GBPMKIRA2OQW2XZZQUCQILI5TMVZ6JNRKM423BSAISDM7ZFWQ6KWEBC4
+    KeyPair source = KeyPair.fromSecretSeed("SCH27VUZZ6UAKB67BDNF6FA42YMBMQCBKXWGMFD5TZ6S5ZZCZFLRXKHS");
+    KeyPair destination = KeyPair.fromAccountId("GDW6AUTBXTOC7FIKUO5BOO3OGLK4SF7ZPOBLMQHMZDI45J2Z6VXRB5NR");
+
+    try {
+      Account account = new Account(source, 2908908335136768L);
+      new Transaction.Builder(account)
+              .addOperation(new CreateAccountOperation.Builder(destination, "2000").build())
+              .addMemo(Memo.none())
+              .addMemo(Memo.none());
+      fail();
+    } catch (RuntimeException exception) {
+      assertTrue(exception.getMessage().contains("Memo has been already added."));
+    }
+  }
+}
