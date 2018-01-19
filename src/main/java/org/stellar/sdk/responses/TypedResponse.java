c5d614d293b8faa2da265e51e568517e37204525
David Clark <davejohnclark@gmail.com>
2018-01-19 22:30:09 +0000

    preserve type information for generic response types

---
+++ src/main/java/org/stellar/sdk/responses/TypedResponse.java
@@ -0,0 +1,14 @@
+package org.stellar.sdk.responses;
+
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * Indicates a generic container that requires type information to be provided after initialisation.
+ *
+ * @param <T> the type of the objects in this response container.
+ */
+public interface TypedResponse<T> {
+
+  void setType(TypeToken<T> type);
+
+}
