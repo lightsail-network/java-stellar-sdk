3594a33c63c53a9b8921d2448d291e3aafa85e16
Bartek Nowotarski <bartek@nowotarski.info>
2018-06-06 12:34:22 +0200

    Add missing effects

---
+++ src/main/java/org/stellar/sdk/responses/effects/DataCreatedEffectResponse.java
@@ -0,0 +1,11 @@
+package org.stellar.sdk.responses.effects;
+
+/**
+ * Represents data_created effect response.
+ * @see <a href="https://www.stellar.org/developers/horizon/reference/resources/effect.html" target="_blank">Effect documentation</a>
+ * @see org.stellar.sdk.requests.EffectsRequestBuilder
+ * @see org.stellar.sdk.Server#effects()
+ */
+public class DataCreatedEffectResponse extends EffectResponse {
+    //
+}
