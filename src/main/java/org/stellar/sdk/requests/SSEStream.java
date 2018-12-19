lightsail-network/java-stellar-sdk

commit 198cab5bc7d4216755011186070c2210bce868d9

use okhttp-sse and drop jersey dependency (#147)

src/main/java/org/stellar/sdk/requests/SSEStream.java
@@ -1,0 +1,193 @@
+package org.stellar.sdk.requests;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.internal.sse.RealEventSource;
+import okhttp3.sse.EventSource;
+import okhttp3.sse.EventSourceListener;
+import org.stellar.sdk.responses.GsonSingleton;
+
+import javax.annotation.Nullable;
+import java.io.Closeable;
+import java.net.SocketException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+
+public class SSEStream<T extends org.stellar.sdk.responses.Response> implements Closeable {
+  private final OkHttpClient okHttpClient;
+  private final RequestBuilder requestBuilder;
+  private final Class<T> responseClass;
+  private final EventListener<T> listener;
+
+  private final AtomicBoolean isStopped = new AtomicBoolean(false);
+  private final AtomicBoolean serverSideClosed = new AtomicBoolean(true); // make sure we start correctly
+  private final AtomicReference<String> lastEventId = new AtomicReference<String>(null);
+  private ExecutorService executorService;
+  private EventSource eventSource = null;
+  private final Lock lock = new ReentrantLock();
+
+  private SSEStream(final OkHttpClient okHttpClient, final RequestBuilder requestBuilder, final Class<T> responseClass, final EventListener<T> listener) {
+    this.okHttpClient = okHttpClient;
+    this.requestBuilder = requestBuilder;
+    this.responseClass = responseClass;
+    this.listener = listener;
+
+
+    executorService = Executors.newSingleThreadExecutor();
+    requestBuilder.buildUri(); // call this once to add the segments
+  }
+
+  private void start() {
+    if (isStopped.get()) {
+      throw new IllegalStateException("Already stopped");
+    }
+    executorService.submit(new Runnable() {
+      @Override
+      public void run() {
+        while (!isStopped.get()) {
+          try {
+            Thread.sleep(200);
+            if (serverSideClosed.get()) {
+              // don't restart until true again
+              serverSideClosed.set(false);
+              if (!isStopped.get()) {
+                lock.lock();
+                try {
+                  // check again if somebody called close in between
+                  if (!isStopped.get()) {
+                    restart();
+                  }
+                } finally {
+                  lock.unlock();
+                }
+              }
+            }
+          } catch (InterruptedException e) {
+            throw new IllegalStateException("interrupted", e);
+          }
+        }
+      }
+    });
+  }
+
+  public String lastPagingToken() {
+    return lastEventId.get();
+  }
+
+  private void restart() {
+    eventSource = doStreamRequest(this,okHttpClient, requestBuilder, responseClass, listener, requestBuilder.uriBuilder.build().toString(), new CloseListener() {
+      @Override
+      public void closed(EventSource source) {
+        serverSideClosed.set(true);
+      }
+    });
+  }
+
+
+  public void close() {
+    isStopped.set(true);
+    if (eventSource != null) {
+      eventSource.cancel();
+    }    executorService.shutdownNow();
+  }
+
+  static <T extends org.stellar.sdk.responses.Response> SSEStream<T> create(
+          final OkHttpClient okHttpClient,
+          final RequestBuilder requestBuilder,
+          final Class<T> responseClass,
+          final EventListener<T> listener) {
+    SSEStream<T> stream = new SSEStream<T>(okHttpClient, requestBuilder, responseClass, listener);
+    stream.start();
+    return stream;
+  }
+
+  private static <T extends org.stellar.sdk.responses.Response> EventSource doStreamRequest(
+          final SSEStream<T> stream,
+          final OkHttpClient okHttpClient,
+          final RequestBuilder requestBuilder,
+          final Class<T> responseClass,
+          final EventListener<T> listener,
+          String url,
+          final CloseListener closeListener) {
+
+    Request.Builder builder = new Request.Builder()
+            .url(url)
+            .header("Accept", "text/event-stream");
+    String lastEventId = stream.lastEventId.get();
+    if(lastEventId != null) {
+      builder.header("Last-Event-ID", lastEventId);
+    }
+    Request request = builder
+            .build();
+    RealEventSource eventSource = new RealEventSource(request, new StellarEventSourceListener<T>(stream,closeListener, responseClass, requestBuilder, listener));
+    eventSource.connect(okHttpClient);
+    return eventSource;
+  }
+
+  private interface CloseListener {
+    void closed(EventSource source);
+  }
+
+  private static class StellarEventSourceListener<T extends org.stellar.sdk.responses.Response> extends EventSourceListener {
+
+    private SSEStream<T> stream;
+    private final CloseListener closeListener;
+    private final Class<T> responseClass;
+    private final RequestBuilder requestBuilder;
+    private final EventListener<T> listener;
+
+    StellarEventSourceListener(SSEStream<T> stream, CloseListener closeListener, Class<T> responseClass, RequestBuilder requestBuilder, EventListener<T> listener) {
+      this.stream = stream;
+      this.closeListener = closeListener;
+      this.responseClass = responseClass;
+      this.requestBuilder = requestBuilder;
+      this.listener = listener;
+    }
+
+    @Override
+    public void onClosed(EventSource eventSource) {
+      if (closeListener != null) {
+        closeListener.closed(eventSource);
+      }
+    }
+
+    @Override
+    public void onOpen(EventSource eventSource, Response response) {
+    }
+
+    @Override
+    public void onFailure(EventSource eventSource, @Nullable Throwable t, @Nullable Response response) {
+      int code = -1;
+      if (response != null) {
+        code = response.code();
+      }
+      if (t != null) {
+        if (t instanceof SocketException) {
+          // not a failure, server disconnected
+        } else {
+          throw new IllegalStateException("Failed " + code, t);
+        }
+      } else {
+        throw new IllegalStateException("Failed " + code);
+      }
+    }
+
+    @Override
+    public void onEvent(EventSource eventSource, @Nullable String id, @Nullable String type, String data) {
+      if (data.equals("\"hello\"") || data.equals("\"goodbye\"")) {
+        return;
+      }
+      T event = GsonSingleton.getInstance().fromJson(data, responseClass);
+      String pagingToken = event.getPagingToken();
+      requestBuilder.cursor(pagingToken);
+      stream.lastEventId.set(id);
+      listener.onEvent(event);
+    }
+  }
+}

build.gradle
@@ -1,4 +1,5 @@
 buildscript {
+  ext.okhttpclientVersion="3.11.0"
   repositories {
     jcenter()
   }
@@ -38,18 +39,17 @@ repositories {
 }
 
 dependencies {
-    compile 'com.squareup.okhttp3:okhttp:3.11.0'
+    compile "com.squareup.okhttp3:okhttp:${okhttpclientVersion}"
+    compile "com.squareup.okhttp3:okhttp-sse:${okhttpclientVersion}"
     compile 'com.moandjiezana.toml:toml4j:0.7.2'
     // use the android version because we don't want java 8 stuff
     compile 'com.google.guava:guava:26.0-android'
     compile 'com.google.code.gson:gson:2.8.5'
     compile 'commons-io:commons-io:2.6'
-    compile 'org.glassfish.jersey.core:jersey-client:2.27'
-    compile 'org.glassfish.jersey.media:jersey-media-sse:2.27'
     compile 'net.i2p.crypto:eddsa:0.3.0'
 
     testCompile 'org.mockito:mockito-core:2.21.0'
-    testCompile 'com.squareup.okhttp3:mockwebserver:3.11.0'
+    testCompile "com.squareup.okhttp3:mockwebserver:${okhttpclientVersion}"
     testCompile 'junit:junit:4.12'
 }
 
@@ -78,21 +78,11 @@ shadowJar {
    archiveName 'stellar-sdk.jar' 
    relocate 'com','shadow.com'
    relocate 'net','shadow.net'
-   relocate 'org.glassfish','shadow.org.glassfish'
-   relocate 'org.mockito','shadow.org.mockito'
-   relocate 'org.aopalliance','shadow.org.aopalliance'
+   relocate 'javax.annotation', 'shadow.javax.annotation'
    relocate 'org.apache','shadow.org.apache'
    relocate 'org.jvnet','shadow.org.jvnet'
-   relocate 'org.objenesis','shadow.org.objenesis'
    relocate 'org.codehaus','shadow.org.codehaus'
    relocate 'org.checkerframework','shadow.org.checkerframework'
-   relocate 'javassist','shadow.javassist'
-   relocate 'javax.validation', 'shadow.javax.validation'
-   relocate 'javax.annotation', 'shadow.javax.annotation'
-   relocate 'javax.inject', 'shadow.javax.inject'
-   relocate 'javax.ws', 'shadow.javax.inject'
-   relocate 'commons-io', 'shadow.commons-io'
-   relocate 'jersey', 'shadow.jersey'
    relocate 'okhttp3','shadow.okhttp3'
    relocate 'okio','shadow.okio'
 }

src/main/java/org/stellar/sdk/requests/AccountsRequestBuilder.java
@@ -1,24 +1,15 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
 import org.stellar.sdk.KeyPair;
 import org.stellar.sdk.responses.AccountResponse;
-import org.stellar.sdk.responses.GsonSingleton;
 import org.stellar.sdk.responses.Page;
 
 import java.io.IOException;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 
 /**
  * Builds requests connected to accounts.
@@ -81,21 +72,9 @@ public class AccountsRequestBuilder extends RequestBuilder {
    * @param listener {@link EventListener} implementation with {@link AccountResponse} type
    * @return EventSource object, so you can <code>close()</code> connection when not needed anymore
    */
-  public EventSource stream(final EventListener<AccountResponse> listener) {
-    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target(this.buildUri().uri());
-    EventSource eventSource = new EventSource(target) {
-      @Override
-      public void onEvent(InboundEvent inboundEvent) {
-        String data = inboundEvent.readData(String.class);
-        if (data.equals("\"hello\"")) {
-          return;
-        }
-        AccountResponse account = GsonSingleton.getInstance().fromJson(data, AccountResponse.class);
-        listener.onEvent(account);
-      }
-    };
-    return eventSource;
+  public SSEStream<AccountResponse> stream(final EventListener<AccountResponse> listener) {
+
+    return SSEStream.create(httpClient,this,AccountResponse.class,listener);
   }
 
   /**

src/main/java/org/stellar/sdk/requests/EffectsRequestBuilder.java
@@ -1,24 +1,15 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
 import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.responses.GsonSingleton;
 import org.stellar.sdk.responses.Page;
 import org.stellar.sdk.responses.effects.EffectResponse;
 
 import java.io.IOException;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
@@ -99,21 +90,8 @@ public class EffectsRequestBuilder extends RequestBuilder {
    * @param listener {@link EventListener} implementation with {@link EffectResponse} type
    * @return EventSource object, so you can <code>close()</code> connection when not needed anymore
    */
-  public EventSource stream(final EventListener<EffectResponse> listener) {
-    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target(this.buildUri().uri());
-    EventSource eventSource = new EventSource(target) {
-      @Override
-      public void onEvent(InboundEvent inboundEvent) {
-        String data = inboundEvent.readData(String.class);
-        if (data.equals("\"hello\"")) {
-          return;
-        }
-        EffectResponse effect = GsonSingleton.getInstance().fromJson(data, EffectResponse.class);
-        listener.onEvent(effect);
-      }
-    };
-    return eventSource;
+  public SSEStream<EffectResponse> stream(final EventListener<EffectResponse> listener) {
+    return SSEStream.create(httpClient,this,EffectResponse.class,listener);
   }
 
   /**

src/main/java/org/stellar/sdk/requests/LedgersRequestBuilder.java
@@ -1,23 +1,14 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
-import org.stellar.sdk.responses.GsonSingleton;
 import org.stellar.sdk.responses.LedgerResponse;
 import org.stellar.sdk.responses.Page;
 
 import java.io.IOException;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 
 /**
  * Builds requests connected to ledgers.
@@ -80,21 +71,9 @@ public class LedgersRequestBuilder extends RequestBuilder {
    * @param listener {@link EventListener} implementation with {@link LedgerResponse} type
    * @return EventSource object, so you can <code>close()</code> connection when not needed anymore
    */
-  public EventSource stream(final EventListener<LedgerResponse> listener) {
-    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target(this.buildUri().uri());
-    EventSource eventSource = new EventSource(target) {
-      @Override
-      public void onEvent(InboundEvent inboundEvent) {
-        String data = inboundEvent.readData(String.class);
-        if (data.equals("\"hello\"")) {
-          return;
-        }
-        LedgerResponse ledger = GsonSingleton.getInstance().fromJson(data, LedgerResponse.class);
-        listener.onEvent(ledger);
-      }
-    };
-    return eventSource;
+
+  public SSEStream<LedgerResponse> stream(final EventListener<LedgerResponse> listener) {
+    return SSEStream.create(httpClient,this,LedgerResponse.class,listener);
   }
 
   /**

src/main/java/org/stellar/sdk/requests/OffersRequestBuilder.java
@@ -1,23 +1,14 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
 import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.responses.GsonSingleton;
 import org.stellar.sdk.responses.OfferResponse;
 import org.stellar.sdk.responses.Page;
-import org.stellar.sdk.responses.operations.OperationResponse;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import java.io.IOException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -57,6 +48,8 @@ public class OffersRequestBuilder extends RequestBuilder {
 
     return responseHandler.handleResponse(response);
   }
+
+
 
   /**
    * Build and execute request.

src/main/java/org/stellar/sdk/requests/OperationsRequestBuilder.java
@@ -1,22 +1,14 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
 import org.stellar.sdk.KeyPair;
-import org.stellar.sdk.responses.GsonSingleton;
 import org.stellar.sdk.responses.Page;
 import org.stellar.sdk.responses.operations.OperationResponse;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import java.io.IOException;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -114,21 +106,8 @@ public class OperationsRequestBuilder extends RequestBuilder {
    * @param listener {@link OperationResponse} implementation with {@link OperationResponse} type
    * @return EventSource object, so you can <code>close()</code> connection when not needed anymore
    */
-  public EventSource stream(final EventListener<OperationResponse> listener) {
-    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target(this.buildUri().uri());
-    EventSource eventSource = new EventSource(target) {
-      @Override
-      public void onEvent(InboundEvent inboundEvent) {
-        String data = inboundEvent.readData(String.class);
-        if (data.equals("\"hello\"")) {
-          return;
-        }
-        OperationResponse operation = GsonSingleton.getInstance().fromJson(data, OperationResponse.class);
-        listener.onEvent(operation);
-      }
-    };
-    return eventSource;
+  public SSEStream<OperationResponse> stream(final EventListener<OperationResponse> listener) {
+    return SSEStream.create(httpClient,this,OperationResponse.class,listener);
   }
 
   /**

src/main/java/org/stellar/sdk/requests/OrderBookRequestBuilder.java
@@ -1,23 +1,14 @@
 package org.stellar.sdk.requests;
 
 import com.google.gson.reflect.TypeToken;
-
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import org.glassfish.jersey.media.sse.EventSource;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
 import org.stellar.sdk.Asset;
 import org.stellar.sdk.AssetTypeCreditAlphaNum;
-import org.stellar.sdk.responses.GsonSingleton;
-import org.stellar.sdk.responses.OfferResponse;
 import org.stellar.sdk.responses.OrderBookResponse;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
 import java.io.IOException;
 
 /**
@@ -68,21 +59,8 @@ public class OrderBookRequestBuilder extends RequestBuilder {
    * @param listener {@link OrderBookResponse} implementation with {@link OrderBookResponse} type
    * @return EventSource object, so you can <code>close()</code> connection when not needed anymore
    */
-  public EventSource stream(final EventListener<OrderBookResponse> listener) {
-    Client client = ClientBuilder.newBuilder().register(SseFeature.class).build();
-    WebTarget target = client.target(this.buildUri().uri());
-    EventSource eventSource = new EventSource(target) {
-      @Override
-      public void onEvent(InboundEvent inboundEvent) {
-        String data = inboundEvent.readData(String.class);
-        if (data.equals("\"hello\"")) {
-          return;
-        }
-        OrderBookResponse orderBook = GsonSingleton.getInstance().fromJson(data, OrderBookResponse.class);
-        listener.onEvent(orderBook);
-      }
-    };
-    return eventSource;
+  public SSEStream<OrderBookResponse> stream(final EventListener<OrderBookResponse> listener) {
+    return SSEStream.create(httpClient,this,OrderBookResponse.class,listener);
   }
 
   public OrderBookResponse execute() throws IOException, TooManyRequestsException {

commit 72e04462299713d6e86823fb2fb7e64b1add8b8d

Fix streaming issues introduced in 198cab5
Close #163

src/main/java/org/stellar/sdk/requests/SSEStream.java
@@ -13,6 +13,7 @@ import java.io.Closeable;
 import java.net.SocketException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.concurrent.locks.Lock;
@@ -33,7 +34,8 @@ public class SSEStream<T extends org.stellar.sdk.responses.Response> implements 
   private final Lock lock = new ReentrantLock();
 
   private SSEStream(final OkHttpClient okHttpClient, final RequestBuilder requestBuilder, final Class<T> responseClass, final EventListener<T> listener) {
-    this.okHttpClient = okHttpClient;
+    // Create a new client with no read timeout
+    this.okHttpClient = okHttpClient.newBuilder().readTimeout(0, TimeUnit.MILLISECONDS).build();
     this.requestBuilder = requestBuilder;
     this.responseClass = responseClass;
     this.listener = listener;
@@ -180,7 +182,7 @@ public class SSEStream<T extends org.stellar.sdk.responses.Response> implements 
 
     @Override
     public void onEvent(EventSource eventSource, @Nullable String id, @Nullable String type, String data) {
-      if (data.equals("\"hello\"") || data.equals("\"goodbye\"")) {
+      if (data.equals("\"hello\"") || data.equals("\"byebye\"")) {
         return;
       }
       T event = GsonSingleton.getInstance().fromJson(data, responseClass);