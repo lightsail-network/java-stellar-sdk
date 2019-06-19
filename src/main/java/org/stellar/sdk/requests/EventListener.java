b3813b6a1e67c7ba270330ee344e86891fcda01f
tamirms <tamir@stellar.org>
2019-06-19 20:46:47 +0300

    Add callback to handle SSE failures (#225)

--- src/main/java/org/stellar/sdk/requests/EventListener.java
+++ src/main/java/org/stellar/sdk/requests/EventListener.java
@@ -1,5 +1,8 @@
 package org.stellar.sdk.requests;
 
+
+import com.google.common.base.Optional;
+
 /**
  * This interface is used in {@link RequestBuilder} classes <code>stream</code> method.
  */
@@ -9,4 +12,12 @@ public interface EventListener<T> {
    * @param object object deserialized from the event data
    */
   void onEvent(T object);
+
+
+  /**
+   * This method will be called when the stream encounters an error.
+   * @param error exception which caused the failure
+   * @param responseCode the HTTP status code from the event stream response
+   * */
+  void onFailure(Optional<Throwable> error, Optional<Integer> responseCode);
 }
