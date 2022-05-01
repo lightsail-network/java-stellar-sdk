a2c879d5478b011c0e35221b7d8e1f4f51dc2152
vinamogit <vincent.amouret@gmail.com>
2022-05-01 22:35:12 +0200

    Basic implementation of liquidity_pools?account

--- src/test/java/org/stellar/sdk/requests/LiquidityPoolsRequestBuilderTest.java
+++ src/test/java/org/stellar/sdk/requests/LiquidityPoolsRequestBuilderTest.java
@@ -26,4 +26,13 @@ public class LiquidityPoolsRequestBuilderTest {
         .buildUri();
     assertEquals("https://horizon-testnet.stellar.org/liquidity_pools?reserves=EURT%3AGAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S%2CPHP%3AGAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S", uri.toString());
   }
+  
+  @Test
+  public void testForAccount() {
+    Server server = new Server("https://horizon-testnet.stellar.org");
+    HttpUrl uri = server.liquidityPools()
+        .forAccount("GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S")
+        .buildUri();
+    assertEquals("https://horizon-testnet.stellar.org/liquidity_pools?account=GAP5LETOV6YIE62YAM56STDANPRDO7ZFDBGSNHJQIYGGKSMOZAHOOS2S", uri.toString());
+  }
 }
