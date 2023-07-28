96813a4a29b85a9269506bd00e48de36d2f73ef7
overcat <4catcode@gmail.com>
2023-07-28 19:56:33 +0800

    add test

--- src/test/java/org/stellar/sdk/SorobanServerTest.java
+++ src/test/java/org/stellar/sdk/SorobanServerTest.java
@@ -1,8 +1,11 @@
 package org.stellar.sdk;
 
+import static org.junit.Assert.assertEquals;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.IOException;
+import java.util.Collections;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
@@ -10,12 +13,196 @@ import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 import org.jetbrains.annotations.NotNull;
 import org.junit.Test;
+import org.stellar.sdk.requests.sorobanrpc.GetLedgerEntriesRequest;
 import org.stellar.sdk.requests.sorobanrpc.GetTransactionRequest;
 import org.stellar.sdk.requests.sorobanrpc.SorobanRpcErrorResponse;
 import org.stellar.sdk.requests.sorobanrpc.SorobanRpcRequest;
+import org.stellar.sdk.responses.sorobanrpc.GetHealthResponse;
+import org.stellar.sdk.responses.sorobanrpc.GetNetworkResponse;
 
 public class SorobanServerTest {
   private final Gson gson = new Gson();
+
+  @Test
+  public void testGetAccount()
+      throws IOException, SorobanRpcErrorResponse, AccountNotFoundException {
+    String accountId = "GDAT5HWTGIU4TSSZ4752OUC4SABDLTLZFRPZUJ3D6LKBNEPA7V2CIG54";
+    String json =
+        "{\n"
+            + "    \"jsonrpc\": \"2.0\",\n"
+            + "    \"id\": \"ecb18f82ec12484190673502d0486b98\",\n"
+            + "    \"result\": {\n"
+            + "        \"entries\": [\n"
+            + "            {\n"
+            + "                \"key\": \"AAAAAAAAAADBPp7TMinJylnn+6dQXJACNc15LF+aJ2Py1BaR4P10JA==\",\n"
+            + "                \"xdr\": \"AAAAAAAAAADBPp7TMinJylnn+6dQXJACNc15LF+aJ2Py1BaR4P10JAAAABdIcDhpAAADHAAAAAwAAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAAAAAABAAAAAAAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAAAAAAAAAAAADAAAAAAABfI8AAAAAZMK3qQ==\",\n"
+            + "                \"lastModifiedLedgerSeq\": \"97423\"\n"
+            + "            }\n"
+            + "        ],\n"
+            + "        \"latestLedger\": \"108023\"\n"
+            + "    }\n"
+            + "}";
+
+    MockWebServer mockWebServer = new MockWebServer();
+    Dispatcher dispatcher =
+        new Dispatcher() {
+          @NotNull
+          @Override
+          public MockResponse dispatch(@NotNull RecordedRequest recordedRequest)
+              throws InterruptedException {
+            GetLedgerEntriesRequest expectedRequest =
+                new GetLedgerEntriesRequest(
+                    Collections.singletonList(
+                        "AAAAAAAAAADBPp7TMinJylnn+6dQXJACNc15LF+aJ2Py1BaR4P10JA=="));
+            SorobanRpcRequest<GetLedgerEntriesRequest> sorobanRpcRequest =
+                gson.fromJson(
+                    recordedRequest.getBody().readUtf8(),
+                    new TypeToken<SorobanRpcRequest<GetLedgerEntriesRequest>>() {}.getType());
+            if ("POST".equals(recordedRequest.getMethod())
+                && sorobanRpcRequest.getMethod().equals("getLedgerEntries")
+                && sorobanRpcRequest.getParams().equals(expectedRequest)) {
+              return new MockResponse().setResponseCode(200).setBody(json);
+            }
+            return new MockResponse().setResponseCode(404);
+          }
+        };
+    mockWebServer.setDispatcher(dispatcher);
+    mockWebServer.start();
+
+    HttpUrl baseUrl = mockWebServer.url("");
+    SorobanServer server = new SorobanServer(baseUrl.toString());
+    TransactionBuilderAccount resp = server.getAccount(accountId);
+    assertEquals(resp.getAccountId(), accountId);
+    assertEquals(resp.getSequenceNumber().longValue(), 3418793967628L);
+    server.close();
+    mockWebServer.close();
+  }
+
+  @Test(expected = AccountNotFoundException.class)
+  public void testGetAccountNotFoundThrows()
+      throws IOException, SorobanRpcErrorResponse, AccountNotFoundException {
+    String accountId = "GBG6OSICP2YJ5ROY4HBGNSVRQDCQ4RYPFFUH6I6BI7LHYNW2CM7AJVBE";
+    String json =
+        "{\n"
+            + "    \"jsonrpc\": \"2.0\",\n"
+            + "    \"id\": \"0376b51e6a744dd2abb3b83be4c2e6dd\",\n"
+            + "    \"result\": {\n"
+            + "        \"entries\": null,\n"
+            + "        \"latestLedger\": \"109048\"\n"
+            + "    }\n"
+            + "}";
+
+    MockWebServer mockWebServer = new MockWebServer();
+    Dispatcher dispatcher =
+        new Dispatcher() {
+          @NotNull
+          @Override
+          public MockResponse dispatch(@NotNull RecordedRequest recordedRequest)
+              throws InterruptedException {
+            SorobanRpcRequest<GetLedgerEntriesRequest> sorobanRpcRequest =
+                gson.fromJson(
+                    recordedRequest.getBody().readUtf8(),
+                    new TypeToken<SorobanRpcRequest<GetLedgerEntriesRequest>>() {}.getType());
+            if ("POST".equals(recordedRequest.getMethod())
+                && sorobanRpcRequest.getMethod().equals("getLedgerEntries")) {
+              return new MockResponse().setResponseCode(200).setBody(json);
+            }
+            return new MockResponse().setResponseCode(404);
+          }
+        };
+    mockWebServer.setDispatcher(dispatcher);
+    mockWebServer.start();
+
+    HttpUrl baseUrl = mockWebServer.url("");
+    SorobanServer server = new SorobanServer(baseUrl.toString());
+    server.getAccount(accountId);
+    server.close();
+    mockWebServer.close();
+  }
+
+  @Test
+  public void testGetHealth() throws IOException, SorobanRpcErrorResponse {
+    String json =
+        "{\n"
+            + "    \"jsonrpc\": \"2.0\",\n"
+            + "    \"id\": \"198cb1a8-9104-4446-a269-88bf000c2721\",\n"
+            + "    \"result\": {\n"
+            + "        \"status\": \"healthy\"\n"
+            + "    }\n"
+            + "}";
+
+    MockWebServer mockWebServer = new MockWebServer();
+    Dispatcher dispatcher =
+        new Dispatcher() {
+          @NotNull
+          @Override
+          public MockResponse dispatch(@NotNull RecordedRequest recordedRequest)
+              throws InterruptedException {
+            SorobanRpcRequest<Void> sorobanRpcRequest =
+                gson.fromJson(
+                    recordedRequest.getBody().readUtf8(),
+                    new TypeToken<SorobanRpcRequest<Void>>() {}.getType());
+            if ("POST".equals(recordedRequest.getMethod())
+                && sorobanRpcRequest.getMethod().equals("getHealth")) {
+              return new MockResponse().setResponseCode(200).setBody(json);
+            }
+            return new MockResponse().setResponseCode(404);
+          }
+        };
+    mockWebServer.setDispatcher(dispatcher);
+    mockWebServer.start();
+
+    HttpUrl baseUrl = mockWebServer.url("");
+    SorobanServer server = new SorobanServer(baseUrl.toString());
+    GetHealthResponse resp = server.getHealth();
+    assertEquals(resp.getStatus(), "healthy");
+    server.close();
+    mockWebServer.close();
+  }
+
+  @Test
+  public void testGetNetwork() throws IOException, SorobanRpcErrorResponse {
+    String json =
+        "{\n"
+            + "    \"jsonrpc\": \"2.0\",\n"
+            + "    \"id\": \"198cb1a8-9104-4446-a269-88bf000c2721\",\n"
+            + "    \"result\": {\n"
+            + "        \"friendbotUrl\": \"http://localhost:8000/friendbot\",\n"
+            + "        \"passphrase\": \"Standalone Network ; February 2017\",\n"
+            + "        \"protocolVersion\": \"20\"\n"
+            + "    }\n"
+            + "}";
+
+    MockWebServer mockWebServer = new MockWebServer();
+    Dispatcher dispatcher =
+        new Dispatcher() {
+          @NotNull
+          @Override
+          public MockResponse dispatch(@NotNull RecordedRequest recordedRequest)
+              throws InterruptedException {
+            SorobanRpcRequest<Void> sorobanRpcRequest =
+                gson.fromJson(
+                    recordedRequest.getBody().readUtf8(),
+                    new TypeToken<SorobanRpcRequest<Void>>() {}.getType());
+            if ("POST".equals(recordedRequest.getMethod())
+                && sorobanRpcRequest.getMethod().equals("getNetwork")) {
+              return new MockResponse().setResponseCode(200).setBody(json);
+            }
+            return new MockResponse().setResponseCode(404);
+          }
+        };
+    mockWebServer.setDispatcher(dispatcher);
+    mockWebServer.start();
+
+    HttpUrl baseUrl = mockWebServer.url("");
+    SorobanServer server = new SorobanServer(baseUrl.toString());
+    GetNetworkResponse resp = server.getNetwork();
+    assertEquals(resp.getFriendbotUrl(), "http://localhost:8000/friendbot");
+    assertEquals(resp.getPassphrase(), "Standalone Network ; February 2017");
+    assertEquals(resp.getProtocolVersion().longValue(), 20L);
+    server.close();
+    mockWebServer.close();
+  }
 
   @Test
   public void testGetTransaction() throws IOException, SorobanRpcErrorResponse {
@@ -53,7 +240,6 @@ public class SorobanServerTest {
                     new TypeToken<SorobanRpcRequest<GetTransactionRequest>>() {}.getType());
             if ("POST".equals(recordedRequest.getMethod())
                 && sorobanRpcRequest.getMethod().equals("getTransaction")
-                && sorobanRpcRequest.getParams().getHash().equals(hash)
                 && sorobanRpcRequest.getParams().equals(expectedRequest)) {
               return new MockResponse().setResponseCode(200).setBody(json);
             }
