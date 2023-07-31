edfda081fe491b58bc0d9753c1b01146b1c9caf2
overcat <4catcode@gmail.com>
2023-07-31 11:43:32 +0800

    Add some APIs to make it more convenient for users to construct InvokeHostFunction.

--- src/main/java/org/stellar/sdk/InvokeHostFunctionOperation.java
+++ src/main/java/org/stellar/sdk/InvokeHostFunctionOperation.java
@@ -1,16 +1,26 @@
 package org.stellar.sdk;
 
+import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Collection;
+import javax.annotation.Nullable;
 import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.Singular;
 import lombok.Value;
 import lombok.experimental.SuperBuilder;
+import org.stellar.sdk.xdr.ContractExecutable;
+import org.stellar.sdk.xdr.ContractExecutableType;
+import org.stellar.sdk.xdr.ContractIDPreimage;
+import org.stellar.sdk.xdr.ContractIDPreimageType;
+import org.stellar.sdk.xdr.CreateContractArgs;
+import org.stellar.sdk.xdr.Hash;
 import org.stellar.sdk.xdr.HostFunction;
+import org.stellar.sdk.xdr.HostFunctionType;
 import org.stellar.sdk.xdr.InvokeHostFunctionOp;
 import org.stellar.sdk.xdr.OperationType;
 import org.stellar.sdk.xdr.SorobanAuthorizationEntry;
+import org.stellar.sdk.xdr.Uint256;
 
 /**
  * Represents <a
@@ -44,6 +54,115 @@ public class InvokeHostFunctionOperation extends Operation {
         .hostFunction(op.getHostFunction())
         .auth(Arrays.asList(op.getAuth()))
         .build();
+  }
+
+  public static InvokeHostFunctionOperationBuilder<?, ?> uploadContractWasmOperationBuilder(
+      byte[] wasm) {
+    HostFunction hostFunction =
+        new HostFunction.Builder()
+            .discriminant(HostFunctionType.HOST_FUNCTION_TYPE_UPLOAD_CONTRACT_WASM)
+            .wasm(wasm)
+            .build();
+    return builder().hostFunction(hostFunction);
+  }
+
+  public static InvokeHostFunctionOperationBuilder<?, ?> createContractOperationBuilder(
+      String wasmId, Address address, @Nullable byte[] salt) {
+    byte[] wasmIdBytes = Util.hexToBytes(wasmId);
+    return createContractOperationBuilder(wasmIdBytes, address, salt);
+  }
+
+  public static InvokeHostFunctionOperationBuilder<?, ?> createContractOperationBuilder(
+      byte[] wasmId, Address address, @Nullable byte[] salt) {
+    if (salt == null) {
+      salt = new byte[32];
+      new SecureRandom().nextBytes(salt);
+    } else if (salt.length != 32) {
+      throw new IllegalArgumentException("\"salt\" must be 32 bytes long");
+    }
+
+    if (wasmId.length != 32) {
+      throw new IllegalArgumentException("\"wasmId\" must be 32 bytes long");
+    }
+
+    CreateContractArgs createContractArgs =
+        new CreateContractArgs.Builder()
+            .contractIDPreimage(
+                new ContractIDPreimage.Builder()
+                    .discriminant(ContractIDPreimageType.CONTRACT_ID_PREIMAGE_FROM_ADDRESS)
+                    .fromAddress(
+                        new ContractIDPreimage.ContractIDPreimageFromAddress.Builder()
+                            .address(address.toSCAddress())
+                            .salt(new Uint256(salt))
+                            .build())
+                    .build())
+            .executable(
+                new ContractExecutable.Builder()
+                    .discriminant(ContractExecutableType.CONTRACT_EXECUTABLE_WASM)
+                    .wasm_hash(new Hash(wasmId))
+                    .build())
+            .build();
+    HostFunction hostFunction =
+        new HostFunction.Builder()
+            .discriminant(HostFunctionType.HOST_FUNCTION_TYPE_CREATE_CONTRACT)
+            .createContract(createContractArgs)
+            .build();
+    return builder().hostFunction(hostFunction);
+  }
+
+  public static InvokeHostFunctionOperationBuilder<?, ?> deployCreateTokenContractOperationBuilder(
+      Asset asset) {
+    CreateContractArgs createContractArgs =
+        new CreateContractArgs.Builder()
+            .contractIDPreimage(
+                new ContractIDPreimage.Builder()
+                    .discriminant(ContractIDPreimageType.CONTRACT_ID_PREIMAGE_FROM_ASSET)
+                    .fromAsset(asset.toXdr())
+                    .build())
+            .executable(
+                new ContractExecutable.Builder()
+                    .discriminant(ContractExecutableType.CONTRACT_EXECUTABLE_TOKEN)
+                    .build())
+            .build();
+    HostFunction hostFunction =
+        new HostFunction.Builder()
+            .discriminant(HostFunctionType.HOST_FUNCTION_TYPE_CREATE_CONTRACT)
+            .createContract(createContractArgs)
+            .build();
+    return builder().hostFunction(hostFunction);
+  }
+
+  public static InvokeHostFunctionOperationBuilder<?, ?> deployCreateTokenContractOperationBuilder(
+      Address address, @Nullable byte[] salt) {
+    if (salt == null) {
+      salt = new byte[32];
+      new SecureRandom().nextBytes(salt);
+    } else if (salt.length != 32) {
+      throw new IllegalArgumentException("\"salt\" must be 32 bytes long");
+    }
+
+    CreateContractArgs createContractArgs =
+        new CreateContractArgs.Builder()
+            .contractIDPreimage(
+                new ContractIDPreimage.Builder()
+                    .discriminant(ContractIDPreimageType.CONTRACT_ID_PREIMAGE_FROM_ADDRESS)
+                    .fromAddress(
+                        new ContractIDPreimage.ContractIDPreimageFromAddress.Builder()
+                            .address(address.toSCAddress())
+                            .salt(new Uint256(salt))
+                            .build())
+                    .build())
+            .executable(
+                new ContractExecutable.Builder()
+                    .discriminant(ContractExecutableType.CONTRACT_EXECUTABLE_TOKEN)
+                    .build())
+            .build();
+    HostFunction hostFunction =
+        new HostFunction.Builder()
+            .discriminant(HostFunctionType.HOST_FUNCTION_TYPE_CREATE_CONTRACT)
+            .createContract(createContractArgs)
+            .build();
+    return builder().hostFunction(hostFunction);
   }
 
   @Override
