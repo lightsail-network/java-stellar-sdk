88ce7ab149feee615e24701ca97ae034d5ade491
overcat <4catcode@gmail.com>
2023-09-19 08:25:53 +0800

    update android test

--- android_test/app/build.gradle.kts
+++ android_test/app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    implementation(files("libs/stellar-sdk.jar"))
+    implementation(files("libs/stellar-sdk-uber.jar"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.uiautomator:uiautomator:2.3.0-alpha03")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
