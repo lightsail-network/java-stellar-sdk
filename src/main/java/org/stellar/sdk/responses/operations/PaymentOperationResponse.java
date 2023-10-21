b4c6881c443c8e794cb9340956f71b27f66507a3
overcat <4catcode@gmail.com>
2023-10-21 18:42:22 +0800

    update link

--- src/main/java/org/stellar/sdk/responses/operations/PaymentOperationResponse.java
+++ src/main/java/org/stellar/sdk/responses/operations/PaymentOperationResponse.java
@@ -12,8 +12,8 @@ import org.stellar.sdk.responses.MuxedAccount;
 /**
  * Represents Payment operation response.
  *
- * @see <a href="https://developers.stellar.org/api/resources/operations/" target="_blank">Operation
- *     documentation</a>
+ * @see <a href="https://developers.stellar.org/api/horizon/resources/operations"
+ *     target="_blank">Operation documentation</a>
  * @see org.stellar.sdk.requests.OperationsRequestBuilder
  * @see org.stellar.sdk.Server#operations()
  */
