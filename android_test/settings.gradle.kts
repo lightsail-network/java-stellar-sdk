1316ec57e43ce38af3b5eed3030ccc1da517db31
Jun Luo <4catcode@gmail.com>
2024-03-30 10:24:04 +0800

    Support publishing artifacts to Maven Central. (#580)

--- android_test/settings.gradle.kts
+++ android_test/settings.gradle.kts
@@ -10,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
-rootProject.name = "java-stellar-sdk"
+rootProject.name = "stellar-sdk"
 
