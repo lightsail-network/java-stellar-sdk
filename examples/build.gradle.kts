74dffd0ff60f26657b073498e6425bfb51c92ed1
Jun Luo <4catcode@gmail.com>
2024-07-03 21:32:27 +0800

    chore: add example. (#611)

---
+++ examples/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("java")
+    id("com.diffplug.spotless") version "6.24.0"
+
+}
+
+group = "network.lightsail"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
+
+spotless {
+    java {
+        importOrder("java", "javax", "org.stellar")
+        removeUnusedImports()
+        googleJavaFormat()
+    }
+}
+
+dependencies {
+    // Use https://central.sonatype.com/artifact/network.lightsail/stellar-sdk in prod.
+    implementation("com.github.lightsail-network:java-stellar-sdk:master-SNAPSHOT:uber")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
