8da317578e32e87548841ae5947d282acb2c1e02
Jun Luo <4catcode@gmail.com>
2024-07-05 15:27:56 +0800

    WIP

--- src/test/java/org/stellar/sdk/responses/TransactionResponseTest.java
+++ src/test/java/org/stellar/sdk/responses/TransactionResponseTest.java
@@ -5,8 +5,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import org.junit.Test;
 import org.stellar.sdk.Memo;
 
@@ -138,5 +142,66 @@ public class TransactionResponseTest {
     assertEquals(0, transactionResponse.getPreconditions().getTimeBounds().getMinTime());
     assertEquals(1720159290, transactionResponse.getPreconditions().getTimeBounds().getMaxTime());
     assertEquals(Memo.none(), transactionResponse.getMemo());
+  }
+
+  @Test
+  public void testDeserializeWithTransactionFeeBump() throws IOException {
+    String filePath = "src/test/resources/responses/transaction_fee_bump.json";
+    String json = new String(Files.readAllBytes(Paths.get(filePath)));
+    TransactionResponse transactionResponse =
+        GsonSingleton.getInstance().fromJson(json, TransactionResponse.class);
+
+    assertEquals(
+        "01f25077d5924dc2381055b22befacfdb7d6466325e2511d9cc0739d23bf14f0",
+        transactionResponse.getHash());
+    assertEquals(52428726, transactionResponse.getLedger().longValue());
+    assertEquals("2024-07-05T05:23:21Z", transactionResponse.getCreatedAt());
+    assertEquals(
+        "GBYODLTHLR7PPEVS2LSXHXHWSL4OGMEFFIINE23RPWJAWVN6WZXZD25O",
+        transactionResponse.getSourceAccount());
+    assertEquals(200, transactionResponse.getFeeCharged().longValue());
+    assertEquals(2000, transactionResponse.getMaxFee().longValue());
+    assertEquals(1, transactionResponse.getOperationCount().intValue());
+    assertTrue(transactionResponse.getSuccessful());
+    assertEquals("none", transactionResponse.getMemoType());
+
+    // InnerTransaction
+    assertEquals(
+        "e96bb6dfa0085e03c19c8dfb6dff81bb3d00e25e80a3d2f2c1c795f88b2f6b7c",
+        transactionResponse.getInnerTransaction().getHash());
+    assertEquals(
+        new ArrayList<>(
+            Arrays.asList(
+                "HliuoKGum8RFuazkmk5EgOGCnS1J2KPp+wTI4fSinp2tqLrtxdHx6fRmrRXyI8lFiLMhg+rkpP8GJ6vnHxl4BA==",
+                "fJsUGgMib2woJjheT+XsVBkwP3nNV0RFw+mZHqH9md9Ag4wzs7HeJvrZ6A6zbJhRJ97jQIgTRuHXdHT/sBFNBw==")),
+        transactionResponse.getInnerTransaction().getSignatures());
+    assertEquals(500, transactionResponse.getInnerTransaction().getMaxFee().longValue());
+
+    // FeeBumpTransaction
+    assertEquals(
+        "01f25077d5924dc2381055b22befacfdb7d6466325e2511d9cc0739d23bf14f0",
+        transactionResponse.getFeeBumpTransaction().getHash());
+    assertEquals(
+        new ArrayList<>(
+            Collections.singletonList(
+                "Qsnj9i9Hdyd+zIAyhN5nuPoAPvj86+0lYuKa5GRw1RVwmln9pD+yVpa3khXQl9/ClDmQR0zkeueZ0A/+LOBdBw==")),
+        transactionResponse.getFeeBumpTransaction().getSignatures());
+  }
+
+  @Test
+  public void testDeserializeWithMuxedId() throws IOException {
+    String filePath = "src/test/resources/responses/transaction_muxed_id.json";
+    String json = new String(Files.readAllBytes(Paths.get(filePath)));
+    TransactionResponse transactionResponse =
+        GsonSingleton.getInstance().fromJson(json, TransactionResponse.class);
+
+    // TODO: fee account muxed
+    assertEquals(
+        "GA5ZPIVPAXEG5LMHTKF4PJZ52K5YFUUBSHL746TINKJ7GNMG4KYF3S4R",
+        transactionResponse.getSourceAccount());
+    assertEquals(
+        "MA5ZPIVPAXEG5LMHTKF4PJZ52K5YFUUBSHL746TINKJ7GNMG4KYF2AAACOBCKABHCTRYY",
+        transactionResponse.getAccountMuxed());
+    assertEquals(BigInteger.valueOf(21449687443220L), transactionResponse.getAccountMuxedId());
   }
 }
