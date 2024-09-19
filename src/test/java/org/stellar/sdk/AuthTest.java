27cd4e8110ca131ef859a028a7b27eb244a3123c
Jun Luo <4catcode@gmail.com>
2024-09-19 14:25:22 +0800

    fix: fix bug with signing auth entries in multi-sig scenarios

--- src/test/java/org/stellar/sdk/AuthTest.java
+++ src/test/java/org/stellar/sdk/AuthTest.java
@@ -219,7 +219,8 @@ public class AuthTest {
             throw new IllegalArgumentException("Unable to convert preimage to bytes", e);
           }
           byte[] payload = Util.hash(data);
-          return signer.sign(payload);
+          byte[] signature = signer.sign(payload);
+          return new Auth.Signature(signer.getPublicKey(), signature);
         };
     long validUntilLedgerSeq = 654656L;
     Network network = Network.TESTNET;
@@ -319,7 +320,8 @@ public class AuthTest {
             throw new IllegalArgumentException("Unable to convert preimage to bytes", e);
           }
           byte[] payload = Util.hash(data);
-          return signer.sign(payload);
+          byte[] signature = signer.sign(payload);
+          return new Auth.Signature(signer.getPublicKey(), signature);
         };
     long validUntilLedgerSeq = 654656L;
     Network network = Network.TESTNET;
@@ -449,6 +451,12 @@ public class AuthTest {
     String contractId = "CDCYWK73YTYFJZZSJ5V7EDFNHYBG4QN3VUNG2IGD27KJDDPNCZKBCBXK";
     KeyPair signer =
         KeyPair.fromSecretSeed("SAEZSI6DY7AXJFIYA4PM6SIBNEYYXIEM2MSOTHFGKHDW32MBQ7KVO6EN");
+    Auth.Signer entrySigner =
+        preimage -> {
+          byte[] invalidData = new byte[20];
+          byte[] signature = signer.sign(invalidData);
+          return new Auth.Signature(signer.getPublicKey(), signature);
+        };
     long validUntilLedgerSeq = 654656L;
     Network network = Network.TESTNET;
 
@@ -485,7 +493,7 @@ public class AuthTest {
             .build();
 
     try {
-      Auth.authorizeEntry(entry.toXdrBase64(), signer, validUntilLedgerSeq, network);
+      Auth.authorizeEntry(entry.toXdrBase64(), entrySigner, validUntilLedgerSeq, network);
       fail();
     } catch (IllegalArgumentException e) {
       assertEquals("signature does not match payload", e.getMessage());
@@ -556,7 +564,8 @@ public class AuthTest {
             throw new IllegalArgumentException("Unable to convert preimage to bytes", e);
           }
           byte[] payload = Util.hash(data);
-          return signer.sign(payload);
+          byte[] signature = signer.sign(payload);
+          return new Auth.Signature(signer.getPublicKey(), signature);
         };
     long validUntilLedgerSeq = 654656L;
     Network network = Network.TESTNET;
@@ -602,5 +611,199 @@ public class AuthTest {
         SCValType.SCV_VEC);
     assertEquals(
         signedEntry.getCredentials().getAddress().getSignature().getVec().getSCVec().length, 1);
+  }
+
+  @Test
+  public void testSignAuthorizeEntryWithKeypairSignerNotEqualCredentialAddress()
+      throws IOException {
+    String contractId = "CDCYWK73YTYFJZZSJ5V7EDFNHYBG4QN3VUNG2IGD27KJDDPNCZKBCBXK";
+    KeyPair signer =
+        KeyPair.fromSecretSeed("SAEZSI6DY7AXJFIYA4PM6SIBNEYYXIEM2MSOTHFGKHDW32MBQ7KVO6EN");
+    long validUntilLedgerSeq = 654656L;
+    Network network = Network.TESTNET;
+    String credentialAddress = "GADBBY4WFXKKFJ7CMTG3J5YAUXMQDBILRQ6W3U5IWN5TQFZU4MWZ5T4K";
+
+    SorobanCredentials credentials =
+        SorobanCredentials.builder()
+            .discriminant(SorobanCredentialsType.SOROBAN_CREDENTIALS_ADDRESS)
+            .address(
+                SorobanAddressCredentials.builder()
+                    .address(new Address(credentialAddress).toSCAddress())
+                    .nonce(new Int64(123456789L))
+                    .signatureExpirationLedger(new Uint32(new XdrUnsignedInteger(0L)))
+                    .signature(Scv.toVoid())
+                    .build())
+            .build();
+    SorobanAuthorizedInvocation invocation =
+        SorobanAuthorizedInvocation.builder()
+            .function(
+                SorobanAuthorizedFunction.builder()
+                    .discriminant(
+                        SorobanAuthorizedFunctionType.SOROBAN_AUTHORIZED_FUNCTION_TYPE_CONTRACT_FN)
+                    .contractFn(
+                        InvokeContractArgs.builder()
+                            .contractAddress(new Address(contractId).toSCAddress())
+                            .functionName(Scv.toSymbol("increment").getSym())
+                            .args(new SCVal[0])
+                            .build())
+                    .build())
+            .subInvocations(new SorobanAuthorizedInvocation[0])
+            .build();
+    SorobanAuthorizationEntry entry =
+        SorobanAuthorizationEntry.builder()
+            .credentials(credentials)
+            .rootInvocation(invocation)
+            .build();
+
+    SorobanAuthorizationEntry signedEntry =
+        Auth.authorizeEntry(entry, signer, validUntilLedgerSeq, network);
+
+    HashIDPreimage preimage =
+        HashIDPreimage.builder()
+            .discriminant(EnvelopeType.ENVELOPE_TYPE_SOROBAN_AUTHORIZATION)
+            .sorobanAuthorization(
+                HashIDPreimage.HashIDPreimageSorobanAuthorization.builder()
+                    .networkID(new Hash(network.getNetworkId()))
+                    .nonce(new Int64(123456789L))
+                    .invocation(invocation)
+                    .signatureExpirationLedger(
+                        new Uint32(new XdrUnsignedInteger(validUntilLedgerSeq)))
+                    .build())
+            .build();
+    byte[] signature = signer.sign(Util.hash(preimage.toXdrByteArray()));
+    SorobanCredentials expectedCredentials =
+        SorobanCredentials.builder()
+            .discriminant(SorobanCredentialsType.SOROBAN_CREDENTIALS_ADDRESS)
+            .address(
+                SorobanAddressCredentials.builder()
+                    .address(new Address(credentialAddress).toSCAddress())
+                    .nonce(new Int64(123456789L))
+                    .signatureExpirationLedger(
+                        new Uint32(new XdrUnsignedInteger(validUntilLedgerSeq)))
+                    .signature(
+                        Scv.toVec(
+                            Collections.singleton(
+                                Scv.toMap(
+                                    new LinkedHashMap<SCVal, SCVal>() {
+                                      {
+                                        put(
+                                            Scv.toSymbol("public_key"),
+                                            Scv.toBytes(signer.getPublicKey()));
+                                        put(Scv.toSymbol("signature"), Scv.toBytes(signature));
+                                      }
+                                    }))))
+                    .build())
+            .build();
+
+    SorobanAuthorizationEntry expectedEntry =
+        SorobanAuthorizationEntry.builder()
+            .credentials(expectedCredentials)
+            .rootInvocation(invocation)
+            .build();
+    assertEquals(expectedEntry, signedEntry);
+    assertNotSame(entry, signedEntry);
+  }
+
+  @Test
+  public void testSignAuthorizeEntryWithFunctionSignerNotEqualCredentialAddress()
+      throws IOException {
+    String contractId = "CDCYWK73YTYFJZZSJ5V7EDFNHYBG4QN3VUNG2IGD27KJDDPNCZKBCBXK";
+    KeyPair signer =
+        KeyPair.fromSecretSeed("SAEZSI6DY7AXJFIYA4PM6SIBNEYYXIEM2MSOTHFGKHDW32MBQ7KVO6EN");
+    Auth.Signer entrySigner =
+        preimage -> {
+          byte[] data;
+          try {
+            data = preimage.toXdrByteArray();
+          } catch (IOException e) {
+            throw new IllegalArgumentException("Unable to convert preimage to bytes", e);
+          }
+          byte[] payload = Util.hash(data);
+          byte[] signature = signer.sign(payload);
+          return new Auth.Signature(signer.getPublicKey(), signature);
+        };
+    long validUntilLedgerSeq = 654656L;
+    Network network = Network.TESTNET;
+    String credentialAddress = "GADBBY4WFXKKFJ7CMTG3J5YAUXMQDBILRQ6W3U5IWN5TQFZU4MWZ5T4K";
+
+    SorobanCredentials credentials =
+        SorobanCredentials.builder()
+            .discriminant(SorobanCredentialsType.SOROBAN_CREDENTIALS_ADDRESS)
+            .address(
+                SorobanAddressCredentials.builder()
+                    .address(new Address(credentialAddress).toSCAddress())
+                    .nonce(new Int64(123456789L))
+                    .signatureExpirationLedger(new Uint32(new XdrUnsignedInteger(0L)))
+                    .signature(Scv.toVoid())
+                    .build())
+            .build();
+    SorobanAuthorizedInvocation invocation =
+        SorobanAuthorizedInvocation.builder()
+            .function(
+                SorobanAuthorizedFunction.builder()
+                    .discriminant(
+                        SorobanAuthorizedFunctionType.SOROBAN_AUTHORIZED_FUNCTION_TYPE_CONTRACT_FN)
+                    .contractFn(
+                        InvokeContractArgs.builder()
+                            .contractAddress(new Address(contractId).toSCAddress())
+                            .functionName(Scv.toSymbol("increment").getSym())
+                            .args(new SCVal[0])
+                            .build())
+                    .build())
+            .subInvocations(new SorobanAuthorizedInvocation[0])
+            .build();
+    SorobanAuthorizationEntry entry =
+        SorobanAuthorizationEntry.builder()
+            .credentials(credentials)
+            .rootInvocation(invocation)
+            .build();
+
+    SorobanAuthorizationEntry signedEntry =
+        Auth.authorizeEntry(entry, entrySigner, validUntilLedgerSeq, network);
+
+    HashIDPreimage preimage =
+        HashIDPreimage.builder()
+            .discriminant(EnvelopeType.ENVELOPE_TYPE_SOROBAN_AUTHORIZATION)
+            .sorobanAuthorization(
+                HashIDPreimage.HashIDPreimageSorobanAuthorization.builder()
+                    .networkID(new Hash(network.getNetworkId()))
+                    .nonce(new Int64(123456789L))
+                    .invocation(invocation)
+                    .signatureExpirationLedger(
+                        new Uint32(new XdrUnsignedInteger(validUntilLedgerSeq)))
+                    .build())
+            .build();
+    byte[] signature = signer.sign(Util.hash(preimage.toXdrByteArray()));
+    SorobanCredentials expectedCredentials =
+        SorobanCredentials.builder()
+            .discriminant(SorobanCredentialsType.SOROBAN_CREDENTIALS_ADDRESS)
+            .address(
+                SorobanAddressCredentials.builder()
+                    .address(new Address(credentialAddress).toSCAddress())
+                    .nonce(new Int64(123456789L))
+                    .signatureExpirationLedger(
+                        new Uint32(new XdrUnsignedInteger(validUntilLedgerSeq)))
+                    .signature(
+                        Scv.toVec(
+                            Collections.singleton(
+                                Scv.toMap(
+                                    new LinkedHashMap<SCVal, SCVal>() {
+                                      {
+                                        put(
+                                            Scv.toSymbol("public_key"),
+                                            Scv.toBytes(signer.getPublicKey()));
+                                        put(Scv.toSymbol("signature"), Scv.toBytes(signature));
+                                      }
+                                    }))))
+                    .build())
+            .build();
+
+    SorobanAuthorizationEntry expectedEntry =
+        SorobanAuthorizationEntry.builder()
+            .credentials(expectedCredentials)
+            .rootInvocation(invocation)
+            .build();
+    assertEquals(expectedEntry, signedEntry);
+    assertNotSame(entry, signedEntry);
   }
 }
