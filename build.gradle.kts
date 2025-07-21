ef895bce2dbf4e1d307cd3b74ce61b8af6e423e5
Jun Luo <4catcode@gmail.com>
2025-07-21 10:12:25 +0800

    test: try using Kotlin to write test code. (#715)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,20 @@ plugins {
     id("com.github.ben-manes.versions") version "0.51.0"
     id("io.freefair.lombok") version "8.10.2"
     id("com.gradleup.nmcp") version "0.0.9"
+    kotlin("jvm") version "2.2.0"
 }
 
 group = "network.lightsail"
 version = "2.0.0-beta0"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(8)
+    }
+}
+
+kotlin {
+    jvmToolchain(8)
 }
 
 spotless {
@@ -23,6 +29,10 @@ spotless {
         importOrder("java", "javax", "org.stellar")
         removeUnusedImports()
         googleJavaFormat()
+    }
+    kotlin {
+        target("src/test/kotlin/**/*.kt")
+        ktfmt("0.56").googleStyle()
     }
 }
 
@@ -46,6 +56,9 @@ dependencies {
     testImplementation("org.bouncycastle:bcpkix-jdk18on:1.79")  // mock https
     testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.11.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
 }
 
 tasks {
