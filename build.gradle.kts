48bb57411608b2ddc8be05c058361e4069858c05
Jun Luo <4catcode@gmail.com>
2025-07-22 16:49:00 +0800

    add kotest-framework-datatest

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
 
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest:kotest-framework-datatest:5.9.1")
 }
 
 tasks {
